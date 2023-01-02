57a4aa69825620e6c6df14e5e664ce6a58ad59db
Rafael Jusi <rafael@jusi.com.br>
2023-01-02 09:39:42 -0300

    Update Arrow

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['target_postgres'],
     install_requires=[
-        'arrow==0.15.5',
+        'arrow==1.2.3',
         'psycopg2-binary==2.8.5',
         'singer-python==5.9.0'
     ],
